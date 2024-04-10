1dbe3ef5c2851c6ada2abb14fc73e05bf680be90
HoppensTech <bhoppens@csumb.edu>
2024-04-09 22:46:12 -0700

    Removed unused variables.

--- app/src/main/java/com/example/eggert_hoppens_project2/CategoryActivity.java
+++ app/src/main/java/com/example/eggert_hoppens_project2/CategoryActivity.java
@@ -5,23 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.eggert_hoppens_project2.databinding.ActivityCategoryBinding;
 
 public class CategoryActivity extends AppCompatActivity {
     ActivityCategoryBinding binding;
-
-//    String firstCategory = binding.categoryFirstOptionButton.getText().toString();
-//    String secondCategory = binding.categorySecondOptionButton.getText().toString();
-    String firstCategory = "@string/video_games";
-    String secondCategory = "@string/sports";
-
-    public static final String TAG = "Category Selection";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
