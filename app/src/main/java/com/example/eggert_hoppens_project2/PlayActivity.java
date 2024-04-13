cd53589f75014f8bf68e0c58197b7c1e2d17a51f
HoppensTech <bhoppens@csumb.edu>
2024-04-12 21:38:39 -0700

    Added testQuestion2 in AppDataBase

--- app/src/main/java/com/example/eggert_hoppens_project2/PlayActivity.java
+++ app/src/main/java/com/example/eggert_hoppens_project2/PlayActivity.java
@@ -3,25 +3,30 @@ package com.example.eggert_hoppens_project2;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 
+import com.example.eggert_hoppens_project2.DB.AppRepository;
+import com.example.eggert_hoppens_project2.DB.entities.Question;
 import com.example.eggert_hoppens_project2.databinding.ActivityPlayBinding;
 
+import java.util.List;
 import java.util.Locale;
 
 public class PlayActivity extends AppCompatActivity {
-
+    private AppRepository repository;
     private static final String GAME_MODE_NAME = "Game Mode Name";
     private static final String CATEGORY_NAME = "Category_Name_Value_String";
-
     private static String gameModeName = "";
     private static String categoryName = "";
 
     private int score = 0;
-    private int currentQuestionNumber = 0;
+    private int currentQuestionIndex = 0;
 
     String userAnswer = "";
 
@@ -37,14 +42,15 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityPlayBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        repository = AppRepository.getRepository(getApplication());
 
+        //Identifiers for text and buttons for editing
         questionTextView = findViewById(R.id.questionTextView);
         questionNumberTextView = findViewById(R.id.questionNumberTextView);
         ans1 = findViewById(R.id.answerFirstButton);
         ans2 = findViewById(R.id.answerSecondButton);
         ans3 = findViewById(R.id.answerThirdButton);
         ans4 = findViewById(R.id.answerFourthButton);
-
 
         //If the previous activity passed an extra for the selected game mode, show the selected
         //game mode on screen using the playPassedGameModeTextView text view.
@@ -59,9 +65,25 @@ public class PlayActivity extends AppCompatActivity {
             categoryName = getIntent().getStringExtra(CATEGORY_NAME);
             binding.playPassedCategoryTextView.setText(String.format(Locale.US, "Category: %s", categoryName));
         }
+
+        //Set questions to textview and buttons. Needs to come after reading the extra as we use it
+        //to select questions based on category.
+        loadNewQuestion();
+
     }
 
-
+    //Using live data from question table in the database, set the textview and buttons
+    //to the corresponding data. This is very basic as it always puts the correct question
+    //in the same spot. TODO: make the question placement random.
+    public void loadNewQuestion(){
+        LiveData<List<Question>> categoryObserver = repository.getAllQuestionsByCategory(categoryName);
+        categoryObserver.observe(this, questionList -> {
+            if(!questionList.isEmpty()){
+                questionTextView.setText(questionList.get(currentQuestionIndex).getQuestion());
+                ans1.setText(questionList.get(currentQuestionIndex).getQuestion());
+            }
+        });
+    }
 
     /**
      * PlayActivity intent factory inteded to be used within GameModeActivity. Utilizes the context
