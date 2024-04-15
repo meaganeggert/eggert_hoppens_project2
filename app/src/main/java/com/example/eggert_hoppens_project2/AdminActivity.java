d60f8ae2ac8f34ec7462c96ebd6ab106f0295d02
meaganeggert <meagan.eggert@gmail.com>
2024-04-14 22:17:08 -0700

    Updating Admin Settings

--- app/src/main/java/com/example/eggert_hoppens_project2/AdminActivity.java
+++ app/src/main/java/com/example/eggert_hoppens_project2/AdminActivity.java
@@ -1,25 +1,44 @@
 package com.example.eggert_hoppens_project2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LiveData;
 
+import com.example.eggert_hoppens_project2.DB.AppRepository;
+import com.example.eggert_hoppens_project2.DB.entities.UserInfo;
 import com.example.eggert_hoppens_project2.databinding.ActivityAdminBinding;
 import com.example.eggert_hoppens_project2.databinding.ActivityCategoryBinding;
 
 public class AdminActivity extends AppCompatActivity {
     ActivityAdminBinding binding;
+    private AppRepository repository;
+
+    private String loggedInUser = "bob";
+    private int loggedInUserId = -1;
+    private static final int LOGGED_OUT = -1;
+    private static final String LOGGED_OUT_USERNAME = "EGGHOP";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAdminBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        checkLoggedInUser();
+
+        // Show persistent UserName
+        TextView toolbar_UserName = (TextView) findViewById(R.id.toolbarUsername);
+        toolbar_UserName.setText(loggedInUser);
 
         //Back Button Functionality
         binding.adminBackButton.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +48,32 @@ public class AdminActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    /**
+     * This method will check to see which user is logged in and display persistent information appropriately
+     */
+    private void checkLoggedInUser() {
+        LiveData<UserInfo> userObserver;
+
+        // check shared preference for logged in user
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(LandingActivity.SHARED_PREFERENCE_USERID_KEY, Context.MODE_PRIVATE);
+        loggedInUserId = sharedPreferences.getInt(LandingActivity.SHARED_PREFERENCE_USERID_KEY, LOGGED_OUT);
+        loggedInUser = sharedPreferences.getString(LandingActivity.SHARED_PREFERENCE_USERNAME_KEY, LOGGED_OUT_USERNAME);
+
+        if (loggedInUserId != LOGGED_OUT) {
+            return;
+        }
+
+        userObserver = repository.getUserByUserId(loggedInUserId);
+        userObserver.observe(this, userInfo -> {
+            if (userInfo != null) {
+                return;
+            }
+        });
+    }
+
+    public static Intent intentFactory(Context context){
+        return new Intent(context, AdminActivity.class);
     }
 }
